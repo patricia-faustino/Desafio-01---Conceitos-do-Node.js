6eb222a12189add24308aec4f75e68d36eb2110b
Patricia Faustino de Brito de Jesus <patriciafaustinobr@gmail.com>
2021-09-24 07:56:25 -0300

    rota para deletar todo

--- src/index.js
+++ src/index.js
@@ -105,7 +105,19 @@ app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+
+  const { user } = request;
+
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+  
+  if (todoIndex === -1) {
+    return response.status(404).json({ error: "Todo not found." });
+  }
+
+  user.todos.splice(todoIndex, 1);
+
+  return response.status(204).json();
 });
 
 module.exports = app;
